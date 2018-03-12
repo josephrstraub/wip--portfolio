7fa58103d775809147e3d6ee0e4be19579c5271c
Joseph Straub <josephrstraub@gmail.com>
2018-03-11 23:12:53 -0400

    Add .scss module support in storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,13 +6,34 @@
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   plugins: [
     // your custom plugins
+    new ExtractTextPlugin({ filename: 'styles.css', allChunks: true })
   ],
   module: {
     rules: [
       // add your custom rules.
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 2,
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              }
+            },
+            'sass-loader'
+          ]
+        })
+      }
     ],
   },
 };
