f092ef18b61c9345387f8ee54d947ae4543feac3
Joseph Straub <josephrstraub@gmail.com>
2018-03-26 09:43:39 -0400

    Remove default landing page

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,18 +1,6 @@
 // @flow
 import React from 'react';
-import logo from 'assets/logo.svg';
-import styles from './styles.scss';
 
 export default function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <h1 className={styles['App-title']}>Welcome to React</h1>
-      </header>
-      <p className={['App-intro']}>
-        To get started, edit <code>src/components/App/index.jsx</code> and save to reload.
-      </p>
-    </div>
-  );
+  return <div />;
 }
